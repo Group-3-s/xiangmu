e88a32a9f8f0068c8a45c42f9c58c2fa49270708
li yue <14858918+li-muzhi1@user.noreply.gitee.com>
2024-09-29 10:41:35 +0800

    解决提示框的问题3

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,27 +1,42 @@
 <template>
-  <van-popup v-model="showPopup" position="bottom" :style="{ height: '50%' }">
-    <div class="custom-popup-content">
-      <h3>自定义标题</h3>
-      <p>这里是弹框的内容，可以自定义。</p>
-      <button @click="showPopup = false">关闭弹框</button>
-    </div>
-  </van-popup>
-  <button @click="showPopup = true">打开弹框</button>
+  <div>
+    <van-dialog v-model:show="show" show-cancel-button>
+      <div class="w-[83vw] h-[90vw] content">
+        <div
+          class="w-[83vw] h-[13vw] mt-[5vw] ml-[6vw] text-xl font-bold text-black"
+        >
+          服务条款和隐私政策提示
+        </div>
+        <p class="ml-[6vw]">欢迎使用网易云音乐!</p>
+        <div class="w-[83vw] h-[80vw] ml-[3vw] pl-[3vw]">
+          <span class="inline-block mt-[5vw]">
+            在您使用网易云音乐前，请您认真阅读并了解
+            <a href="">《服务条款》</a>和<a href=""> 《隐私政策》</a>。
+          </span>
+          <span class="inline-block mt-[5vw]">
+            如您未满14周岁,您还需通知您的监护人共同阅读
+            <a href="">《儿童隐私政策》</a>， 点击“同
+            意”即表示您和您的监护人已阅读并同意全部条款。
+          </span>
+        </div>
+      </div>
+    </van-dialog>
+  </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { Popup } from "vant";
-import { onMounted } from "vue";
-const showPopup = ref(false);
-onMounted(() => {
-  showPopup.value = true;
-});
+import { Dialog } from "vant";
+import { ref, onMounted } from "vue";
+
+const show = ref(true);
 </script>
 
 <style scoped>
-.custom-popup-content {
-  padding: 20px;
-  text-align: center;
+.content {
+  font-size: 18px;
+  color: gray;
+}
+a {
+  color: #4a7eb8;
 }
 </style>
