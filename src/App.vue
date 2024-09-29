e744eefee6755d6bf305b3eb96118d0c95dc9c9f
cui_yu_lin <1239303157@qq.com>
2024-09-29 08:33:15 +0800

    保存

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <RouterView />
   <!-- <div class="w-[100px] h-[100px] bg-red-400"></div> -->
 
-  <div class="bg-[#f1f1f1]"><HomePage /></div>
+  <!-- <div class="bg-[#f1f1f1]"><HomePage /></div> -->
 </template>
 <script setup>
-import HomePage from "@/views/HomPage.vue";
+// import HomePage from "@/views/HomPage.vue";
 </script>
 
 <style></style>
